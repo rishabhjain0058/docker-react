417d7f1215817aeaea52c0a9df3f9e4775cbe402
rishabhjain0058 <root@ip-172-31-89-111.ec2.internal>
2020-04-16 20:07:21 +0000

    commit to developer branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-Hello value got chnaged from git
+Hello value got changed from git
         </p>
         <a
           className="App-link"
